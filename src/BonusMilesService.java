bd58d4d9cacfffca77a7b1ce6f6a5fecda7dcd5f
Василий Шкурихин <virtualshkus@yandex.ru>
2022-06-08 15:28:34 +0500

    createcalculator

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    public int calculate(int coast) {
+
+        int costMile = 20;
+        int miles = coast / costMile;
+
+        return miles;
+    }
+
+
+}
